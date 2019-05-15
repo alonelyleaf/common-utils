22df50745665eef230e8251a4f1875a1d108fdb0
bijl <bijl@yealink.com>
2019-05-15 19:36:48 +0800

    project init

---
+++ common-guava/src/main/java/com/alonelyleaf/guava/CommonGuavaApplication.java
@@ -0,0 +1,13 @@
+package com.alonelyleaf.guava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonGuavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonGuavaApplication.class, args);
+    }
+
+}
