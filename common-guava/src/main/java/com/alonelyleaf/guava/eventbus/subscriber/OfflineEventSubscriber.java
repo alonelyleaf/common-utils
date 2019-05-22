d25493750634f17aac9a9d87a537608e20297807
bijl <bijl@yealink.com>
2019-05-22 21:04:24 +0800

    add test for guava's eventBus

--- common-guava/src/main/java/com/alonelyleaf/guava/eventbus/subscriber/OfflineEventSubscriber.java
+++ common-guava/src/main/java/com/alonelyleaf/guava/eventbus/subscriber/OfflineEventSubscriber.java
@@ -3,15 +3,21 @@ package com.alonelyleaf.guava.eventbus.subscriber;
 import com.alonelyleaf.guava.base.ApplicationContextProvider;
 import com.alonelyleaf.guava.eventbus.event.OfflineEvent;
 import com.alonelyleaf.guava.eventbus.eventbus.EventBusCenter;
+import com.alonelyleaf.guava.util.JSONUtils;
 import com.google.common.eventbus.AllowConcurrentEvents;
 import com.google.common.eventbus.Subscribe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
  * @author bijl
  * @date 2019/5/15
  */
+@Component
 public class OfflineEventSubscriber {
+
+    private static Logger logger = LoggerFactory.getLogger(UploadEventSubscriber.class);
 
     private EventBusCenter busCenter;
 
@@ -37,5 +43,6 @@ public class OfflineEventSubscriber {
 
     private void dealWith(OfflineEvent event){
 
+        logger.info("OfflineEventSubscriber receive event {}", JSONUtils.serialize(event));
     }
 }
--- common-guava/src/test/java/com/alonelyleaf/guava/CommonGuavaApplicationTests.java
+++ common-guava/src/test/java/com/alonelyleaf/guava/CommonGuavaApplicationTests.java
@@ -1,7 +1,10 @@
 package com.alonelyleaf.guava;
 
+import com.alonelyleaf.guava.eventbus.event.OfflineEvent;
+import com.alonelyleaf.guava.eventbus.publisher.EventPublisher;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CommonGuavaApplicationTests {
 
+    @Autowired
+    private EventPublisher eventPublisher;
+
     @Test
-    public void contextLoads() {
+    public void pushOfflineEvent(){
+
+        String accountId = "123";
+        Long offlineTime = 1558527237L;
+
+        OfflineEvent offlineEvent = new OfflineEvent(accountId, offlineTime);
+
+        eventPublisher.publishOfflineEvent(offlineEvent);
+
+        System.out.println("accountId = " + accountId + "offlineTime = " + offlineTime);
     }
 
 }
