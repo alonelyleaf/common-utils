63b7306a37ba8961d17e3fa2687d29fd75fb8884
bijl <bijl@yealink.com>
2020-05-07 16:37:03 +0800

    快速排序修改

--- common-algorithm/src/main/java/com/alonelyleaf/algorithm/sort/QuickSort.java
+++ common-algorithm/src/main/java/com/alonelyleaf/algorithm/sort/QuickSort.java
@@ -20,6 +20,31 @@ public class QuickSort<T extends Comparable<T>> extends Sort<T> {
         sort(nums, 0, nums.length - 1);
     }
 
+    /**
+     * 基于快速排出，实现查找第K大元素
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public T select(T[] nums, int k) {
+        int l = 0, h = nums.length - 1;
+        while (h > l) {
+            int j = partition(nums, l, h);
+
+            if (j == k) {
+                return nums[k];
+
+            } else if (j > k) {
+                h = j - 1;
+
+            } else {
+                l = j + 1;
+            }
+        }
+        return nums[k];
+    }
+
     private void sort(T[] nums, int l, int h) {
         if (h <= l)
             return;
@@ -44,7 +69,7 @@ public class QuickSort<T extends Comparable<T>> extends Sort<T> {
                 break;
             swap(nums, i, j);  //交换两个数，以保证左右两个组内的数分别小于和大于基准值
         }
-        swap(nums, l, j);
-        return j;
+        swap(nums, l, j); //将基准值交换到中间位置，并且左侧分组均小于基准值，右侧均大于基准值
+        return j; //返回基准值所在位置
     }
 }
