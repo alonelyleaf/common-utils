f23bc9d5cbe87a640dcd451309cf01e1db7ee2c6
bijl <bijl1332981668@gmail.com>
2021-06-29 23:15:57 +0800

    feat: 剑指offer 29、50

---
+++ common-algorithm/src/main/java/com/alonelyleaf/algorithm/offer/FirstNotRepeatingChar50.java
@@ -0,0 +1,60 @@
+package com.alonelyleaf.algorithm.offer;
+
+import java.util.BitSet;
+
+/**
+ * 第一个只出现一次的字符位置
+ *
+ * @author bijl
+ * @date 6/29/21
+ */
+public class FirstNotRepeatingChar50 {
+
+    /**
+     * 最直观的解法是使用 HashMap 对出现次数进行统计：字符做为 key，出现次数作为 value，遍历字符串每次都将 key 对应的 value 加 1。最后再遍历这个 HashMap 就可以找出出现次数为 1 的字符。
+     * <p>
+     * 考虑到要统计的字符范围有限，也可以使用整型数组代替 HashMap。ASCII 码只有 128 个字符，因此可以使用长度为 128 的整型数组来存储每个字符出现的次数。
+     *
+     * @param str
+     * @return
+     */
+    public int firstNotRepeatingChar1(String str) {
+        int[] cnts = new int[128];
+        for (int i = 0; i < str.length(); i++) {
+            cnts[str.charAt(i)]++;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (cnts[str.charAt(i)] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * 以上实现的空间复杂度还不是最优的。考虑到只需要找到只出现一次的字符，那么需要统计的次数信息只有 0,1,更大，使用两个比特位就能存储这些信息。
+     *
+     * @param str
+     * @return
+     */
+    public int firstNotRepeatingChar2(String str) {
+
+        BitSet bs1 = new BitSet(128);
+        BitSet bs2 = new BitSet(128);
+        for (char c : str.toCharArray()) {
+            if (!bs1.get(c) && !bs2.get(c)) {
+                bs1.set(c);     // 0 0 -> 0 1
+            } else if (bs1.get(c) && !bs2.get(c)) {
+                bs2.set(c);     // 0 1 -> 1 1
+            }
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (bs1.get(c) && !bs2.get(c))  // 0 1
+                return i;
+        }
+        return -1;
+    }
+}
---
+++ common-algorithm/src/main/java/com/alonelyleaf/algorithm/offer/PrintMatrix29.java
@@ -0,0 +1,56 @@
+package com.alonelyleaf.algorithm.offer;
+
+import java.util.ArrayList;
+
+/**
+ * 顺时针打印矩阵
+ *
+ * @author bijl
+ * @date 6/29/21
+ */
+public class PrintMatrix29 {
+
+    /**
+     * 一层一层从外到里打印，观察可知每一层打印都有相同的处理步骤，唯一不同的是上下左右的边界不同了。因此使用四个变量 r1, r2, c1, c2 分别存储上下左右边界值，
+     * 从而定义当前最外层。打印当前最外层的顺序：从左到右打印最上一行->从上到下打印最右一行->从右到左打印最下一行->从下到上打印最左一行。应当注意只有在 r1 != r2 时才打印最下一行，
+     * 也就是在当前最外层的行数大于 1 时才打印最下一行，这是因为当前最外层只有一行时，继续打印最下一行，会导致重复打印。打印最左一行也要做同样处理。
+     *
+     * @param matrix
+     * @return
+     * @link http://www.cyc2018.xyz/算法/剑指%20Offer%20题解/29.%20顺时针打印矩阵.html#题目描述
+     */
+    public ArrayList<Integer> printMatrix(int[][] matrix) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int r1 = 0, r2 = matrix.length - 1;
+        int c1 = 0, c2 = matrix[0].length - 1;
+        while (r1 <= r2 && c1 <= c2) {
+            for (int i = c1; i <= c2; i++) {
+                result.add(matrix[r1][i]);
+            }
+
+            for (int i = r1 + 1; i <= r2; i++) {
+                result.add(matrix[i][c2]);
+            }
+
+            if (r1 != r2) {
+                for (int i = c2 - 1; i >= c1; i--) {
+                    result.add(matrix[r2][i]);
+                }
+            }
+
+            if (c1 != c2) {
+                for (int i = r2 - 1; i > r1; i--) {
+                    result.add(matrix[i][c1]);
+                }
+            }
+
+            r1++;
+            c1++;
+            r2--;
+            c2--;
+        }
+
+        return result;
+    }
+
+}
